44f2d961d1eb456eaccfbc9d3aec0286ccb8c133
Joel Howard <joelhoward2@gmail.com>
2022-12-18 12:05:57 -0500

    rubocop refactor

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,40 +1,41 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 def clean_phone_numbers(phone_num)
-  #remove non-numeric chars:
-  phone_num = phone_num.gsub(/[^0-9]/, "")
-  #modify numbers per lesson instructions:
-  if phone_num.length < 10 || phone_num.length > 11 || (phone_num.length == 11 && phone_num[0] != "1")
-    phone_num = "0000000000"
-  elsif phone_num.length == 11 && phone_num[0] == "1"
+  # remove non-numeric chars:
+  phone_num = phone_num.gsub(/[^0-9]/, '')
+  # modify numbers per lesson instructions:
+  if phone_num.length < 10 || phone_num.length > 11 || (phone_num.length == 11 && phone_num[0] != '1')
+    phone_num = '0000000000'
+  elsif phone_num.length == 11 && phone_num[0] == '1'
     phone_num.slice!(0)
   end
-  #reformat phone number:
-  phone_num = "(#{phone_num[0..2]}) #{phone_num[3..5]}-#{phone_num[6..9]}"
+  # reformat phone number:
+  "(#{phone_num[0..2]}) #{phone_num[3..5]}-#{phone_num[6..9]}"
 end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
-
   begin
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -56,8 +57,8 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 def parse_time(time, hours, days)
-  date_registered = Time.strptime(time, "%m/%d/%y %H:%M")
-  day_registered = date_registered.strftime("%A")
+  date_registered = Time.strptime(time, '%m/%d/%y %H:%M')
+  day_registered = date_registered.strftime('%A')
   hour_registered = date_registered.hour
   hours << hour_registered
   days << day_registered
@@ -73,17 +74,17 @@ contents.each do |row|
   phone_num = clean_phone_numbers(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
-  save_thank_you_letter(id,form_letter)
+  save_thank_you_letter(id, form_letter)
   puts "name: #{name}, zipcode: #{zipcode}, phone number: #{phone_num}"
   parse_time(row[:regdate], hours, days)
 end
 
 def sort_by_frequency(times)
-  times.tally.sort_by {|k, v| v}.reverse.to_h
+  times.tally.sort_by { |_k, v| v }.reverse.to_h
 end
 
 hours_frequencies = sort_by_frequency(hours)
-hours_frequencies.transform_keys! { |key| key.to_s + ':00'}
+hours_frequencies.transform_keys! { |key| "#{key}:00" }
 
 days_frequencies = sort_by_frequency(days)
 
@@ -94,5 +95,5 @@ def print_frequencies(title, frequencies)
   end
 end
 
-print_frequencies("hour", hours_frequencies)
-print_frequencies("day", days_frequencies)
+print_frequencies('hour', hours_frequencies)
+print_frequencies('day', days_frequencies)
